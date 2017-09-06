3bd096171c23c9cccf38c2138d46759cc1305c2e
François Rosato <francois.rosato@ekino.com>
2017-09-06 11:54:32 +0200

    [+] Service Giphy - Search method

---
+++ src/services/giphy.js
@@ -0,0 +1,29 @@
+import Api from './../utils/api';
+
+let configInstance = null;
+
+// @TODO MOVE TO CONFIG OR .ENV
+const BASE_URL = 'http://api.giphy.com';
+const API_KEY = '87ff4ebc68774374b2f22f18d5c9cdd5';
+const FORMAT = 'json';
+
+export default class GiphySerice {
+
+  /**
+   * search - Call the /search endPoint
+   *
+   * @param  {String} query the key word to search
+   * @param  {String} offset An optional results offset. Defaults to 0.
+   * @param  {type} limit The maximum number of records to return. Defaults to 25
+   * @return {Promise} the result of the API Call
+   */
+  static search(query, offset = 0, limit = 25){
+    const endPoint = `${BASE_URL}/v1/gifs/search`;
+    return Api.get(endPoint, {
+      q: query,
+      limit: limit,
+      api_key: API_KEY,
+      fmt: FORMAT,
+    });
+  }
+}
