2a249a333b31b985f49303aa828d25d320acb729
François Rosato <francois.rosato@ekino.com>
2017-09-06 11:55:28 +0200

    [+] Dom helper - Ready + Get

---
+++ src/utils/dom/index.js
@@ -0,0 +1,59 @@
+/**
+ * getById
+ *
+ * @param  {String} id
+ * @return {DomElement}
+ */
+const getById = id => document.getElementById(id);
+
+/**
+ * getByClass
+ *
+ * @param  {String} classname
+ * @return {DomElements}
+ */
+const getByClass = classname => document.getElementsByClassName(classname);
+
+/**
+ * getByTag
+ *
+ * @param  {String} tag
+ * @return {DomElements}
+ */
+const getByTag = tag => document.getElementsByTagName(tag);
+
+export default class Dom {
+
+  /**
+   * get
+   *
+   * @param  {String} id/class
+   * @return {DomElement(s)}
+   */
+  static get(selector) {
+    console.log('selector', selector.substr(1));
+    if(selector.startsWith('#')) {
+      return getById(selector.substr(1))
+    }
+    if (selector.startsWith('.')) {
+      return getByClass(selector.substr(1))
+    }
+    return getElementsByTagName(selector)
+  }
+
+  /**
+   * ready
+   *
+   * @return {Promise}
+   */
+  static ready(callback) {
+    return new Promise((resolve, reject) => {
+      document.onreadystatechange = function () {
+        // I use a callback not a promise to copy JQuery DocumentReady method
+        if (document.readyState == "interactive") {
+            resolve();
+        }
+      }
+    });
+  }
+}
